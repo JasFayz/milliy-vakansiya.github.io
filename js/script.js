e3a1da30e110206d838b9c8c418c38dcceffd3b8
Jasur <jasfayz@gmail.com>
2021-09-30 23:18:47 +0500

    Add registration page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 $(document).ready(function () {
 
+    console.log($(document).width())
 
-    if ($(window).width() < 991) {
-        $('.mobile__button button').click(function () {
-            $('#nav_menu').toggleClass('show')
+    if ($(document).width() < 776) {
+        $('.mobile__btn button').click(function () {
+            console.log('click');
+            $('.main_nav').toggleClass('show')
+        })
+        $('.js_close_main-nav').click(function () {
+            $('.main_nav').removeClass('show')
         })
     }
 
+
+    $('.js_popup').click(function (e) {
+        let popup = $('.map_popup');
+        let regionName = $(e.delegateTarget).attr('data-name');
+        popup.find('.title b').text(regionName);
+        var left = (e.originalEvent.pageX - popup.width() / 2 - 15) + 'px';
+        var top = (e.originalEvent.pageY - popup.height() - 50) + 'px';
+        popup.css({
+            top: top,
+            left: left
+        })
+
+        popup.addClass('show');
+        $('.map').mouseleave(function () {
+            setTimeout(function () {
+                $('.map_popup').removeClass('show');
+            }, 2000)
+        })
+    })
 
     $('.type-carousel').owlCarousel({
         items: 2,
@@ -14,11 +38,12 @@ $(document).ready(function () {
         margin: 10,
         autoWidth: true,
         dots: false,
-        nav: true,
+        nav: false,
         navText: ["<div class='prev-btn'><img src='./image/icon_prev-btn.svg' width='30'></div>", "<div class='next-btn'><img src='./image/icon_next-btn.svg' width='30'></div>"],
         responsive: {
             600: {
-                items: 6
+                items: 6,
+                nav: true,
             }
         }
     })
@@ -27,8 +52,8 @@ $(document).ready(function () {
         loop: true,
         margin: 32,
         nav: true,
-        items: 4,
-        stagePadding: 0,
+        items: 1,
+        stagePadding: 80,
         dots: false,
         singleItem: true,
         navText: ["<div class='prev-btn'><img src='./image/icon_prev-btn-white.svg' width='32'></div>", "<div class='next-btn'><img src='./image/icon_next-btn-white.svg' width='32'></div>"],
@@ -52,46 +77,74 @@ $(document).ready(function () {
         loop: true,
         margin: 32,
         nav: true,
-        items: 2,
-        stagePadding: 0,
+        items: 1,
         autoplay: true,
         dots: false,
         singleItem: true,
+        stagePadding: 80,
+        nav: false,
         navText: ["<div class='prev-btn'><img src='./image/icon_prev-btn-blue.svg' width='32'></div>", "<div class='next-btn'><img src='./image/icon_next-btn-blue.svg' width='32'></div>"],
         responsive: {
             1200: {
-                items: 4
+                items: 4,
+                nav: true,
             },
             991: {
-                items: 3
+                items: 3,
+                nav: true,
+
             },
             776: {
-                items: 2
+                items: 2,
+                nav: true,
+
             },
             556: {
-                items: 1
+                items: 1,
+                nav: false,
             }
         }
     })
 
+    if ($(document).width() < 991) {
+        $('.vacancy__list .row').addClass('owl-carousel owl-theme');
+        $('.employer__list .row').addClass('owl-carousel owl-theme');
+        $('.vacancies__tab .tab-content .row').addClass('owl-carousel');
 
-    $('.js_popup').click(function (e) {
 
-        let popup = $('.map_popup');
-        let regionName = $(e.delegateTarget).attr('data-name');
-        popup.find('.title b').text(regionName);
-        var left = (e.originalEvent.pageX - popup.width() / 2 - 15) + 'px';
-        var top = (e.originalEvent.pageY - popup.height() - 50) + 'px';
-        popup.css({
-            top: top,
-            left: left
+        $('.vacancy__list> .owl-carousel').owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 10,
+            autoWidth: true,
+            dots: false,
+            nav: false,
+            responsive: {
+                600: {
+                    items: 2
+                }
+            }
         })
-
-        popup.addClass('show');
-        $('.map').mouseleave(function () {
-            setTimeout(function () {
-                $('.map_popup').removeClass('show');
-            }, 2000)
+        $('.employer__list> .owl-carousel').owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 10,
+            autoWidth: true,
+            dots: false,
+            nav: false,
         })
-    })
+        $('.vacancies__tab .tab-content .row ').owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 10,
+            autoWidth: true,
+            dots: false,
+            nav: false,
+            center: true
+        })
+    }
+
+
+
+
 })
